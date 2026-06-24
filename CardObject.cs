e055fec60f5370c9581e8d7c45a033b7098b8d61
Azad Ashurov <wertdrof@gmail.com>
2026-06-24 22:41:57 +0400

    card object

---
+++ CardObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DrawIt
+{
+    public  class CardObject
+    {
+       public string Name { get; set; }
+        public string Description { get; set; }
+        public int Cost { get; set; }
+        public int ID { get; set; }
+        public string ImagePath { get; set; }
+        public bool Revealed { get; set; }
+        public bool PowerActive { get; set; }
+        public Enum CardType { get; set; }
+        public Enum CardDiscardingType { get; set; }
+        public object SpecialEffect { get; set; }
+    }
+   
+    }
